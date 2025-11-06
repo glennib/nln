d17d07a87406fc08f9d0036bc5fe0f2fba9cda4a
Glenn Bitar <glennbitar@gmail.com>
2025-11-06 20:17:56 +0100

    style: compact main

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,10 @@ use std::io::{self, BufReader, Write, stdin, stdout};
 use nln::snickerdoodle;
 
 fn main() -> io::Result<()> {
-    let stdin = stdin();
-    let stdin = stdin.lock();
-    let stdin = BufReader::new(stdin);
-
+    let stdin = BufReader::new(stdin().lock());
     let stdout = stdout();
     let mut stdout = stdout.lock();
-
     snickerdoodle(stdin, &mut stdout)?;
     stdout.flush()?;
-
     Ok(())
 }
